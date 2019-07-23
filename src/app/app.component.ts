22765877681ed0e3f71c305debe4d8bf486d42c9
Victor Abedi <v.nyangweso@carepay.co.ke>
2019-07-23 08:39:19 -0400

    v1 basic understanding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+interface AppState {
+  message$: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngrx-angular';
+  message$: Observable<string>;
+
+  constructor(private store: Store<AppState>) {
+    this.message$ = this.store.select('message');
+  }
+
+  spanishMessage() {
+    this.store.dispatch({type: 'SPANISH'});
+  }
+  frenchMessage() {
+    this.store.dispatch({type: 'FRENCH'});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { StoreModule } from '@ngrx/store';
+import {simpleReducer} from './simple.reduce';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({message: simpleReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
